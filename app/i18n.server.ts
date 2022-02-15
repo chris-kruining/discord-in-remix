70e4c354ca50afbc0fc000934abb52781bba3ff2
Chris Kruining <c.kruining@fyn.nl>
2022-02-15 11:54:35 +0100

    attempt to fix i18n

--- app/i18n.server.ts
+++ app/i18n.server.ts
@@ -1,7 +1,14 @@
 import { RemixI18Next } from 'remix-i18next';
-import { FileSystemBackend } from 'remix-i18next';
+import { FetchBackend, FileSystemBackend } from 'remix-i18next';
+import os from 'os';
 
-let backend = new FileSystemBackend('./public/locales');
+const ip = Object.values(os.networkInterfaces()).flat().find(ip => ip?.family == "IPv4" && !ip.internal)!
+
+// const backend = new FileSystemBackend('./public/locales');
+const backend = new FetchBackend({
+    baseUrl: new URL(`http://${ip.address}:${process.env.PORT ?? 3000}`),
+    pathPattern: '/locales/:locale/:namespace.json',
+});
 
 export let i18n = new RemixI18Next(backend, {
     fallbackLng: 'en',
