c7de64cd1e3da0380ae16467d0851aef4b058933
Chris Kruining <c.kruining@fyn.nl>
2022-02-15 15:40:35 +0100

    added initial menu styling

--- app/component/menu.tsx
+++ app/component/menu.tsx
@@ -1,8 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faCompass } from '@fortawesome/free-solid-svg-icons';
-import { faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { PropsWithChildren } from 'react';
-import { Link } from 'remix';
+import { NavLink } from 'remix';
 
 type Server = {
     id: number,
@@ -18,15 +18,16 @@ export function Menu({ servers }: MenuProps) {
     return <nav className="main">
         <Item to="/"><FontAwesomeIcon className="home" icon={faDiscord} /></Item>
         <hr />
-        { servers.map(server => <Item key={ server.id } to={`/${server.name}`}>{server.icon ? <img src={server.icon} /> : server.name}</Item>) }
+        { servers.map(server => <Item key={ server.id } to={`/${server.name.replaceAll(' ', '-')}`}>{server.icon ? <img src={server.icon} /> : server.name}</Item>) }
         <Item to="/server/new"><FontAwesomeIcon icon={faPlus} /></Item>
         <Item to="/explore"><FontAwesomeIcon icon={faCompass} /></Item>
+        <a href="https://github.com/chris-kruining/discord-in-remix"><FontAwesomeIcon icon={faGithub} /></a>
     </nav>;
 }
 
-function Item({ to, children }: PropsWithChildren<{ to: string }>)
+function Item({ className = '', to, children }: PropsWithChildren<{ className?: string, to: string }>)
 {
-    return <Link className="main__item" to={to}>
+    return <NavLink className={`main__item ${className}`} to={to}>
         <span>{children}</span>
-    </Link>
+    </NavLink>
 }
