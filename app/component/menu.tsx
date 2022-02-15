1074f3ff65bc30a4f390a9ca19acaa14edf0428e
Chris Kruining <c.kruining@fyn.nl>
2022-02-15 15:49:41 +0100

    actually fix external links this time...

--- app/component/menu.tsx
+++ app/component/menu.tsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faCompass } from '@fortawesome/free-solid-svg-icons';
 import { faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { PropsWithChildren } from 'react';
+import { IntrinsicElements, PropsWithChildren } from 'react';
 import { NavLink } from 'remix';
 
 type Server = {
@@ -21,13 +21,14 @@ export function Menu({ servers }: MenuProps) {
         { servers.map(server => <Item key={ server.id } to={`/${server.name.replace(/ +/g, '-')}`}>{server.icon ? <img src={server.icon} /> : server.name}</Item>) }
         <Item to="/server/new"><FontAwesomeIcon icon={faPlus} /></Item>
         <Item to="/explore"><FontAwesomeIcon icon={faCompass} /></Item>
-        <a href="https://github.com/chris-kruining/discord-in-remix" className="main__item"><FontAwesomeIcon icon={faGithub} /></a>
+        <Item to="https://github.com/chris-kruining/discord-in-remix"><FontAwesomeIcon icon={faGithub} /></Item>
     </nav>;
 }
 
 function Item({ className = '', to, children }: PropsWithChildren<{ className?: string, to: string }>)
 {
-    return <NavLink className={`main__item ${className}`} to={to}>
-        <span>{children}</span>
-    </NavLink>
+    const content = <span>{children}</span>;
+    return to.startsWith('http')
+        ? <a href={to} className={`main__item ${className}`} target="_blank">{content}</a>
+        : <NavLink className={`main__item ${className}`} to={to}>{content}</NavLink>;
 }
