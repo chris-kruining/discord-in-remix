f82c1037a7643ddd8a3d1ab801a12d4ef21d8fed
Chris Kruining <chris@kruining.eu>
2022-02-21 22:07:56 +0100

    attempt 2

--- remix.config.js
+++ remix.config.js
@@ -4,9 +4,5 @@
 module.exports = {
     serverBuildTarget: 'vercel',
     server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-    appDirectory: 'app',
-    assetsBuildDirectory: 'public/build',
-    publicPath: '/build/',
-    serverBuildDirectory: 'api/_build',
     ignoredRouteFiles: [ '.*' ],
 };
